392997ea54f52abc39196332661b5821ebf1e718
victrexx2002 <victrexxmgbemena@gmail.com>
2024-04-05 17:45:34 +0100

    updates

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const http = require('http');
-const bot = require('./bot');
-const port = 3000;
-require("./main")
+// const http = require('http');
+// const bot = require('./bot');
+// const port = 3000;
+// require("./main")
 
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('My Telegram bot is running\n');
-}).listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// http.createServer((req, res) => {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('My Telegram bot is running\n');
+// }).listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
